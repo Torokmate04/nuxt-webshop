76f6773ff0752151d23dd596bd07d152f0d9da6e
Máté Török <torokmate676@gmail.com>
2023-06-05 17:36:24 +0200

    kész_webshop

---
+++ nuxt-webshop/pages/about.vue
@@ -0,0 +1,14 @@
+<script setup>
+useHead({
+  title: 'Rólunk -Nuxtal'
+})
+</script>
+
+<template>
+<div>
+<h1>Ez meg az about</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta asperiores, dolores dolorem, ducimus, libero dolore velit ipsa quis nostrum numquam iure ex voluptatum? Omnis sed praesentium, veniam aperiam nisi facere maiores nostrum, delectus nesciunt commodi illo consequatur esse? Alias, voluptatem.</p>
+</div>
+</template>
+
+<style scoped></style>
